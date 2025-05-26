6096e7b8887a791737ae81a250cd4d65274bbcb6
sagyzdop <sagyzdop+github@gmail.com>
2025-05-26 17:48:47 +0500

    Up to chapter 4.2 (not included)

---
+++ cmd/api/movies.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"greenlight.sagyzdop.com/internal/data"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are a subset
+	// of the Movie struct that we created earlier). This struct will be our *target
+	// decode destination*.
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Initialize a new json.Decoder instance which reads from the request body, and
+	// then use the Decode() method to decode the body contents into the input struct.
+	// Importantly, notice that when we call Decode() we pass a *pointer* to the input
+	// struct as the target decode destination. If there was an error during decoding,
+	// we also use our generic errorResponse() helper to send the client a 400 Bad
+	// Request response containing the error message.
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+    id, err := app.readIDParam(r)
+    if err != nil {
+        // Use the new notFoundResponse() helper.
+        app.notFoundResponse(w, r)
+        return
+    }
+
+    movie := data.Movie{
+        ID:        id,
+        CreatedAt: time.Now(),
+        Title:     "Casablanca",
+        Runtime:   102,
+        Genres:    []string{"drama", "romance", "war"},
+        Version:   1,
+    }
+
+    err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+    if err != nil {
+        // Use the new serverErrorResponse() helper.
+        app.serverErrorResponse(w, r, err)
+    }
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module greenlight.sagyzdop.com
+
+go 1.24.3
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
